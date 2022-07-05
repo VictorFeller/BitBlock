ba1a6b8a2dd9467eab670c86e2e34de5199876a7
Romain <rom.jys@outlook.com>
2022-07-05 11:21:04 +0200

    add dependencies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./main.css";
+//import "bootswatch/dist/darkly/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 
